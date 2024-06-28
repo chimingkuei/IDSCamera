babe1833a4ad82bb3e0122d4b596967454f145e8
chimingkuei <chimingkuei@gmail.com>
2024-06-28 23:14:49 +0800

    IDSCamera

---
+++ IDSCamera/IDS.cs
@@ -0,0 +1,314 @@
+﻿using HandyControl.Controls;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace IDSCamera
+{
+    interface IAOI
+    {
+        OpenCvSharp.Mat Binarization(Bitmap bitmap);
+    }
+
+    class IDS: IAOI
+    {
+        private uEye.Camera m_Camera;
+        private bool m_bLive = false;
+        public IntPtr m_displayHandle = IntPtr.Zero;
+        private const int m_cnNumberOfSeqBuffers = 3;
+        
+        private uEye.Defines.Status AllocImageMems()
+        {
+            uEye.Defines.Status statusRet = uEye.Defines.Status.SUCCESS;
+
+            for (int i = 0; i < m_cnNumberOfSeqBuffers; i++)
+            {
+                statusRet = m_Camera.Memory.Allocate();
+
+                if (statusRet != uEye.Defines.Status.SUCCESS)
+                {
+                    FreeImageMems();
+                }
+            }
+
+            return statusRet;
+        }
+        private uEye.Defines.Status FreeImageMems()
+        {
+            int[] idList;
+            uEye.Defines.Status statusRet = m_Camera.Memory.GetList(out idList);
+
+            if (uEye.Defines.Status.SUCCESS == statusRet)
+            {
+                foreach (int nMemID in idList)
+                {
+                    do
+                    {
+                        statusRet = m_Camera.Memory.Free(nMemID);
+
+                        if (uEye.Defines.Status.SEQ_BUFFER_IS_LOCKED == statusRet)
+                        {
+                            Thread.Sleep(1);
+                            continue;
+                        }
+
+                        break;
+                    }
+                    while (true);
+                }
+            }
+
+            return statusRet;
+        }
+        private uEye.Defines.Status InitSequence()
+        {
+            int[] idList;
+            uEye.Defines.Status statusRet = m_Camera.Memory.GetList(out idList);
+
+            if (uEye.Defines.Status.SUCCESS == statusRet)
+            {
+                statusRet = m_Camera.Memory.Sequence.Add(idList);
+
+                if (uEye.Defines.Status.SUCCESS != statusRet)
+                {
+                    ClearSequence();
+                }
+            }
+
+            return statusRet;
+        }
+        private uEye.Defines.Status ClearSequence()
+        {
+            return m_Camera.Memory.Sequence.Clear();
+        }
+        private void onFrameEvent(object sender, EventArgs e)
+        {
+            uEye.Camera Camera = sender as uEye.Camera;
+
+            Int32 s32MemID;
+            uEye.Defines.Status statusRet = Camera.Memory.GetLast(out s32MemID);
+
+            if ((uEye.Defines.Status.SUCCESS == statusRet) && (0 < s32MemID))
+            {
+                if (uEye.Defines.Status.SUCCESS == Camera.Memory.Lock(s32MemID))
+                {
+                    Camera.Display.Render(s32MemID, m_displayHandle, uEye.Defines.DisplayRenderMode.FitToWindow);
+                    Camera.Memory.Unlock(s32MemID);
+                }
+            }
+        }
+
+        public void InitCamera(int CameraID)
+        {
+            m_Camera = new uEye.Camera();
+            uEye.Defines.Status statusRet = 0;
+
+            // Open Camera
+            statusRet = m_Camera.Init(CameraID);
+            if (statusRet != uEye.Defines.Status.Success)
+            {
+                MessageBox.Show("Camera initializing failed");
+                Environment.Exit(-1);
+            }
+
+            //if (File.Exists(camera_parameter_file))
+            //{
+            //    //Load Camera Parameter File
+            //    statusRet = m_Camera.Parameter.Load(camera_parameter_file);
+            //    if (statusRet != uEye.Defines.Status.Success)
+            //    {
+            //        MessageBox.Show("Loading parameter failed: " + statusRet);
+            //    }
+            //}
+            //else
+            //{
+            //    MessageBox.Show("Please confirm whether the camera file exists!");
+            //}
+
+            // Allocate Memory
+            statusRet = AllocImageMems();
+            if (statusRet != uEye.Defines.Status.Success)
+            {
+                MessageBox.Show("Allocate Memory failed");
+                Environment.Exit(-1);
+            }
+
+            statusRet = InitSequence();
+            if (statusRet != uEye.Defines.Status.Success)
+            {
+                MessageBox.Show("Add to sequence failed");
+                Environment.Exit(-1);
+            }
+
+            // Start Live Video
+            statusRet = m_Camera.Acquisition.Capture();
+            if (statusRet != uEye.Defines.Status.Success)
+            {
+                MessageBox.Show("Start Live Video failed");
+            }
+            else
+            {
+                m_bLive = true;
+            }
+
+            // Connect Event
+            m_Camera.EventFrame += onFrameEvent;
+        }
+
+        public void ContinuousAcquisition()
+        {
+            // Open Camera and Start Live Video
+            if (m_Camera.Acquisition.Capture() == uEye.Defines.Status.Success)
+            {
+                m_bLive = true;
+            }
+        }
+
+        public void StopContinuousAcquisition()
+        {
+            // Stop Live Video
+            if (m_Camera.Acquisition.Stop() == uEye.Defines.Status.Success)
+            {
+                m_bLive = false;
+            }
+        }
+
+        public void FreezeImage()
+        {
+            if (m_Camera.Acquisition.Freeze() == uEye.Defines.Status.Success)
+            {
+                m_bLive = false;
+            }
+        }
+
+        public void LoadParameter(string camera_parameter_file)
+        {
+            uEye.Defines.Status statusRet = 0;
+
+            m_Camera.Acquisition.Stop();
+
+            ClearSequence();
+            FreeImageMems();
+
+            statusRet = m_Camera.Parameter.Load(camera_parameter_file);
+            if (statusRet != uEye.Defines.Status.Success)
+            {
+                MessageBox.Show("Loading parameter failed: " + statusRet);
+            }
+
+            // Allocate Memory
+            statusRet = AllocImageMems();
+            if (statusRet != uEye.Defines.Status.Success)
+            {
+                MessageBox.Show("Allocate Memory failed");
+                Environment.Exit(-1);
+            }
+
+            statusRet = InitSequence();
+            if (statusRet != uEye.Defines.Status.Success)
+            {
+                MessageBox.Show("Add to sequence failed");
+                Environment.Exit(-1);
+            }
+
+            if (m_bLive == true)
+            {
+                m_Camera.Acquisition.Capture();
+            }
+
+        }
+
+        public void Exit()
+        {
+            m_Camera.Exit();
+        }
+
+        public void CameraParameter(int value)
+        {
+            #region Gamma
+            //m_Camera.Gamma.Software.Set(value);
+            //int gamma_max;
+            //int gamma_min;
+            //int gamma_inc;
+            //m_Camera.Gamma.Software.GetRange(out gamma_min, out gamma_max, out gamma_inc);
+            //Console.WriteLine(gamma_min);
+            //Console.WriteLine(gamma_max);
+            #endregion
+            #region Framerate
+            //m_Camera.Timing.Framerate.Set(value);
+            //double framerate_max;
+            //double framerate_min;
+            //double framerate_inc;
+            //m_Camera.Timing.Framerate.GetFrameRateRange(out framerate_min, out framerate_max, out framerate_inc);
+            //Console.WriteLine(framerate_min);
+            //Console.WriteLine(framerate_max);
+            #endregion
+            #region Exposure.
+            //m_Camera.Timing.Exposure.Set(value);
+            //double exposure_max;
+            //double exposure_min;
+            //double exposure_inc;
+            //m_Camera.Timing.Exposure.GetRange(out exposure_max, out exposure_min, out exposure_inc);
+            //Console.WriteLine(exposure_min);
+            //Console.WriteLine(exposure_max);
+            #endregion
+            //m_Camera.Gain.Hardware.Factor.SetMaster(value);
+        }
+
+        public void SaveImage(string image_file, string dip)
+        {
+            // Save Image Method One
+            //m_Camera.Image.Save(@"E:\1.bmp");
+
+            // Save Image Method Two
+            Bitmap bitmap = new Bitmap(2456, 2054, System.Drawing.Imaging.PixelFormat.Format8bppIndexed);
+            if (m_Camera.Memory.ToBitmap(m_cnNumberOfSeqBuffers - 1, out bitmap) == uEye.Defines.Status.Success)
+            {
+                switch (dip)
+                {
+                    case "None":
+                        {
+                            OpenCvSharp.Mat src = OpenCvSharp.Extensions.BitmapConverter.ToMat(bitmap);
+                            OpenCvSharp.Cv2.ImWrite(image_file, src);
+                            break;
+                        }
+                    case "Binarization":
+                        {
+                            OpenCvSharp.Mat Binary = Binarization(bitmap);
+                            OpenCvSharp.Cv2.ImWrite(image_file, Binary);
+                            break;
+                        }
+
+                }
+                
+            }
+        }
+
+        public OpenCvSharp.Mat Binarization(Bitmap bitmap)
+        {
+            OpenCvSharp.Mat src = OpenCvSharp.Extensions.BitmapConverter.ToMat(bitmap);
+            OpenCvSharp.Mat Binary = src.Threshold(100, 255, OpenCvSharp.ThresholdTypes.Binary);
+            return Binary;
+        }
+
+        
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+}
---
+++ IDSCamera/MainWindow.xaml.cs
@@ -0,0 +1,217 @@
+﻿using Microsoft.Win32;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Image = System.Windows.Controls.Image;
+
+namespace IDSCamera
+{
+    public class Parameter
+    {
+        public string Camera_Parameter_File_val { get; set; }
+        public string Save_Image_Path_val { get; set; }
+        public int Gamma_val { get; set; }
+        public int Gain_val { get; set; }
+    }
+    
+    public partial class MainWindow : Window
+    {
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        #region Function
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (HandyControl.Controls.MessageBox.Show("Do you want to close the window?", "確認", MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.Yes)
+            {
+                e.Cancel = false;
+            }
+            else
+            {
+                e.Cancel = true;
+            }
+        }
+
+        private void ChangeIcon(string path, Image name, string tip)
+        {
+            BitmapImage bitmapImage = new BitmapImage();
+            bitmapImage.BeginInit();
+            bitmapImage.UriSource = new Uri(path);
+            bitmapImage.EndInit();
+            name.Source = bitmapImage;
+            name.ToolTip = tip;
+        }
+        #endregion
+
+        #region Parameter and Init
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            UI.m_displayHandle = Display_Windows.Handle;
+            //Load Config
+            List<Parameter> Parameter_info = Config.Load();
+            Camera_Parameter_File.Text = Parameter_info[0].Camera_Parameter_File_val;
+            Save_Image_Path.Text = Parameter_info[0].Save_Image_Path_val;
+            Gamma.Value = Parameter_info[0].Gamma_val;
+            Gain.Value = Parameter_info[0].Gain_val;
+        }
+        BaseLogRecord Logger = new BaseLogRecord();
+        BaseConfig<Parameter> Config = new BaseConfig<Parameter>();
+        private IDS UI = new IDS();
+        bool Freeze_state = true;
+        bool Continuous_Acquisition_state = false;
+        #endregion
+
+        #region Main Screen
+        private void Main_Btn_Click(object sender, RoutedEventArgs e)
+        {
+            switch ((sender as Button).Name)
+            {
+                case nameof(Continuous_Acquisition):
+                    {
+                        if (!Continuous_Acquisition_state)
+                        {
+                            UI.InitCamera(0);
+                            Logger.WriteLog("Continuous Acquisition!", 1, richTextBoxGeneral);
+                            Continuous_Acquisition_state = true;
+                            ChangeIcon(AppDomain.CurrentDomain.BaseDirectory + @"Icon\Stop.png", Continuous_Acquisition_Icon, "停止連續取像");
+                        }
+                        else if (Continuous_Acquisition_state)
+                        {
+                            UI.StopContinuousAcquisition();
+                            UI.Exit();
+                            Display_Windows.Image = null;
+                            Logger.WriteLog("Stop Continuous Acquisition!", 1, richTextBoxGeneral);
+                            Continuous_Acquisition_state = false;
+                            ChangeIcon(AppDomain.CurrentDomain.BaseDirectory + @"Icon\Start.png", Continuous_Acquisition_Icon, "連續取像");
+                        }
+                        break;
+                    }
+                case nameof(Freeze):
+                    {
+                        if (Continuous_Acquisition_state)
+                        {
+                            if (Freeze_state)
+                            {
+                                UI.FreezeImage();
+                                Logger.WriteLog("Freeze Image!", 1, richTextBoxGeneral);
+                                Freeze_state = false;
+                                ChangeIcon(AppDomain.CurrentDomain.BaseDirectory + @"Icon\Unfreeze.png", Freeze_Icon, "恢復取像畫面");
+                            }
+                            else if (!Freeze_state)
+                            {
+                                UI.ContinuousAcquisition();
+                                Logger.WriteLog("Unfreeze Image!", 1, richTextBoxGeneral);
+                                Freeze_state = true;
+                                ChangeIcon(AppDomain.CurrentDomain.BaseDirectory + @"Icon\Freeze.png", Freeze_Icon, "凍結取像畫面");
+                            }
+
+                        }
+                        else
+                        {
+                            HandyControl.Controls.MessageBox.Show("Please check to turn on the camera!", "確認", MessageBoxButton.OK, MessageBoxImage.Warning);
+                        } 
+                        break;
+                    }
+                case nameof(Save_Image):
+                    {
+                        if (Continuous_Acquisition_state)
+                        {
+                            if (!string.IsNullOrEmpty(Save_Image_Path.Text))
+                            {
+                                UI.SaveImage(System.IO.Path.Combine(Save_Image_Path.Text, DateTime.Now.ToString("yyyyMMddHHmmssffff") + ".bmp"),"None");
+                                Logger.WriteLog("Save Image "+ DateTime.Now.ToString("yyyyMMddHHmmssffff") + ".bmp", 1, richTextBoxGeneral);
+                            }
+                            else
+                            {
+                                HandyControl.Controls.MessageBox.Show("Please set the save image path!", "確認", MessageBoxButton.OK, MessageBoxImage.Warning);
+                            }
+                        }
+                        else
+                        {
+                            HandyControl.Controls.MessageBox.Show("Please check to turn on the camera!", "確認", MessageBoxButton.OK, MessageBoxImage.Warning);
+                        }
+                        break;
+                    }
+                case nameof(Load_Parameter):
+                    {
+                        if (Continuous_Acquisition_state)
+                        {
+                            UI.LoadParameter(Camera_Parameter_File.Text);
+                            Logger.WriteLog("Load Parameter!", 1, richTextBoxGeneral);
+                        }
+                        else
+                        {
+                            HandyControl.Controls.MessageBox.Show("Please check to turn on the camera!", "確認", MessageBoxButton.OK, MessageBoxImage.Warning);
+                        }
+                        break;
+                    }
+            }
+        }
+        #endregion
+
+        #region  Camera Parameter Screen
+        private void Camera_Parameter_Btn_Click(object sender, RoutedEventArgs e)
+        {
+            switch ((sender as Button).Name)
+            {
+                case nameof(Open_Camera_Parameter_File):
+                    {
+                        OpenFileDialog camera_parameter_file = new OpenFileDialog();
+                        camera_parameter_file.Title = "Choose Camera Parameter File";
+                        camera_parameter_file.Filter = "Camera Config Files(*.ini)|*.ini";
+                        Nullable<bool> result = camera_parameter_file.ShowDialog();
+                        if (result == true)
+                        {
+                            Camera_Parameter_File.Text = camera_parameter_file.FileName;
+                            Logger.WriteLog("Set Camera Parameter Path!", 1, richTextBoxGeneral);
+                        }
+                        break;
+                    }
+                case nameof(Open_Save_Image_Path):
+                    {
+                        System.Windows.Forms.FolderBrowserDialog save_image_path = new System.Windows.Forms.FolderBrowserDialog();
+                        save_image_path.Description = "Choose Save Image Path";
+                        save_image_path.ShowDialog();
+                        Save_Image_Path.Text = save_image_path.SelectedPath;
+                        break;
+                    }
+                case nameof(Save_Config):
+                    {
+                        List<Parameter> Parameter_config = new List<Parameter>()
+                        {
+                            new Parameter() {Camera_Parameter_File_val=Camera_Parameter_File.Text,
+                                             Save_Image_Path_val=Save_Image_Path.Text,
+                                             Gamma_val=Convert.ToInt32(Gamma.Value),
+                                             Gain_val = Convert.ToInt32(Gain.Value)
+                                            }
+                        };
+                        Config.Save(Parameter_config);
+                        Logger.WriteLog("Save Parameter!", 1, richTextBoxGeneral);
+                        break;
+                    }
+
+            }
+        }
+        #endregion
+
+        
+    }
+}
